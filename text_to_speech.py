dba3b46a03595f0743eae1d0cc002596faddf141
gspreddy6869 <153407405+gspreddy6869@users.noreply.github.com>
2023-12-31 00:59:10 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,25 @@
+# Import the required module for text
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+import os
+
+# The text that you want to convert to audio
+mytext = 'Hi friends THIS IS GOLI SHIVAPRASAD REDDY  I am glad to say that as an python developer is going to I am completed my first project'
+# Language in which you want to convert
+language = 'en'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("speech.mp3")
+
+# Playing the converted file
+os.system("song.mp3")
